215ebe6e8c4438f10ada023a7698f4b4fb875889
Daniil Kuznetsov <dkuznetsov17@gmail.com>
2021-03-26 16:42:03 +0000

    Give me a place to stand and I shall move the world

--- Entity.py
+++ Entity.py
@@ -1,21 +1,45 @@
 import pygame as pg
 import numpy as np
+from Environment3D import SCREEN_SIZE
+from abc import ABC, abstractmethod
 from typing import Tuple
+from Config import *
 
-FOV = 90
 
-class EntityRectangle():
+
+class Entity(ABC):
+    def __init__(self, colour):
+        global FOV, SCREEN_SIZE
+
+        self.dots = []
+
+        self.colour = colour
+        # Calculate the offset of the eye based on the desired vertical field of view
+        self.eyeOffset = (SCREEN_SIZE[1]/2)/(np.tan((np.pi/180) * (FOV/2)))
+
+        
+
+    def move(self, x_dist: float, y_dist: float, z_dist: float) -> None:
+        self.dots += np.array([x_dist, y_dist, z_dist])
+    
+    @abstractmethod
+    def rotate(self, x_rot: float, y_rot: float, z_rot: float) -> None:
+        pass
+
+    @abstractmethod
+    def blit(self, surf: pg.surface.Surface) -> None:
+        pass
+
+
+class Rectangle(Entity):
     def __init__(self, center: Tuple[float, float, float], size: Tuple[float, float, float], angle: Tuple[float, float, float]) -> None:
-        super().__init__()
+        super().__init__((255, 0 ,0))
 
         self.size = np.array([size[0], size[1], size[2]])
-        self.half_size = self.size/2
-        self.center = np.array([center[0], center[1], center[2]])
         
         self.edge_width = 2
-        self.colour = (255, 0, 0)
 
-        self.mask = np.array([
+        self.dots = np.array([
                             [[-1, 0, 0], [0, -1, 0], [0, 0, -1]],
                             [[-1, 0, 0], [0, 1, 0], [0, 0, -1]],
                             [[1, 0, 0], [0, 1, 0], [0, 0, -1]],
@@ -26,21 +50,19 @@ class EntityRectangle():
                             [[-1, 0, 0], [0, -1, 0], [0, 0, 1]],
                             ], dtype=np.float64)
         
-        self.mask_t = self.mask.transpose((0, 2, 1))
+        self.dots = np.array([0, 0, self.eyeOffset]) + center + ((self.size/2) * self.dots)
+
+        self.dots_t = self.dots.transpose((0, 2, 1))
 
         self.rotate(angle[0], angle[1], angle[2])
 
-    def _calculateDots(self) -> np.ndarray:
-        # Draw dots
-        return self.center + (self.half_size * np.sum(self.mask, axis=-2))
-        
-    def move(self, x_dist: float, y_dist: float, z_dist: float) -> None:
-        self.center[0] += x_dist
-        self.center[1] += y_dist
-        self.center[2] += z_dist
+    def _mergeDotVectors(self) -> np.array:
+        return np.sum(self.dots, axis=-2)
 
     # angle == [x_rot, y_rot, z_rot]
     def rotate(self, x_rot: float, y_rot: float, z_rot: float) -> None:
+        
+
         rot_x = np.array([
                         [1, 0, 0],
                         [0, np.cos(x_rot), -np.sin(x_rot)],
@@ -54,28 +76,22 @@ class EntityRectangle():
                         [np.sin(z_rot), np.cos(z_rot), 0],
                         [0, 0, 1]])
 
-        np.matmul((rot_z @ rot_y @ rot_x), self.mask_t, out=self.mask_t)
+        # No need to update dots, as dots_t is a view
+        np.matmul((rot_z @ rot_y @ rot_x), self.dots_t, out=self.dots_t)
+
 
     def blit(self, surf: pg.surface.Surface) -> None:
-        global FOV
+        global SCREEN_SIZE
 
-        # sh == screen half
-        sh_x, sh_y = surf.get_size()[0]/2, surf.get_size()[1]/2
-        # Calculate the offset of the eye based on the desired vertical field of view
-        eye_offset = sh_y/(np.tan((np.pi/180) * (FOV/2)))
-
-        dots = self._calculateDots()
-        
-        # Add the offset to the z axis
-        dots[:, 2] += eye_offset
+        dots = self._mergeDotVectors()
 
         # tan and arctan cancel out
-        dots[:, 1] = eye_offset * (dots[:, 1]/dots[:, 2])
-        dots[:, 0] = eye_offset * (dots[:, 0]/dots[:, 2])
+        dots[:, 1] = self.eyeOffset * (dots[:, 1]/dots[:, 2])
+        dots[:, 0] = self.eyeOffset * (dots[:, 0]/dots[:, 2])
 
         # Shift origin to pygame position
-        dots[:, 1] += sh_y
-        dots[:, 0] += sh_x
+        dots[:, 1] += SCREEN_SIZE[1]/2
+        dots[:, 0] += SCREEN_SIZE[0]/2
         
         # Get rid of z-axis as we have finished projecting points onto screen
         dots = np.delete(dots, 2, -1)
@@ -96,8 +112,6 @@ class EntityRectangle():
             for p1, p2 in zip(edge, edge[1:]):
                 pg.draw.line(surf, (0,0,0), p1, p2, width=self.edge_width)
             pg.draw.line(surf, (0,0,0), edge[0], edge[-1], width=self.edge_width)
-
-
 
 
         
--- Environment3D.py
+++ Environment3D.py
@@ -1,66 +1,86 @@
 import pygame as pg
 import numpy as np
-from typing import Dict, List, Any
-from Entity import EntityRectangle
 
-SCREEN_SIZE = (800, 600)
-ROTATION_ANGLE = 2
-MOVEMENT_DISTANCE = 10
+from Config import *
+from typing import Dict, Any
+from Entity import Rectangle
+
+
 
 class Environment3D:
-    global ROTATION_ANGLE
     def __init__(self) -> None:
-        self.keycodes: Dict[int, List[Any]] = dict([
-                (pg.K_w, [lambda : self._rotate(-np.pi/180 * ROTATION_ANGLE, 0, 0)]),
-                (pg.K_s, [lambda : self._rotate(np.pi/180 * ROTATION_ANGLE,0, 0)]),
-                (pg.K_d, [lambda : self._rotate(0, -np.pi/180 * ROTATION_ANGLE, 0)]),
-                (pg.K_a, [lambda : self._rotate(0, np.pi/180 * ROTATION_ANGLE, 0)]),
-                (pg.K_UP, [lambda : self._move(0, -MOVEMENT_DISTANCE, 0)]),
-                (pg.K_DOWN, [lambda : self._move(0, MOVEMENT_DISTANCE, 0)]), 
-                (pg.K_RIGHT, [lambda : self._move(MOVEMENT_DISTANCE, 0, 0)]),
-                (pg.K_LEFT, [lambda : self._move(-MOVEMENT_DISTANCE, 0, 0)]),
-                (pg.K_q, [lambda : self._move(0, 0, MOVEMENT_DISTANCE)]),
-                (pg.K_e, [lambda : self._move(0, 0, -MOVEMENT_DISTANCE)]), 
+        global ROTATION_ANGLE, MOVEMENT_DISTANCE, SCREEN_SIZE
+        """
+        Keybinds:
+            Rotation:
+                w - Rotate the world about the x-axis in negative direction
+                s - Rotate the world about the x-axis in positive direction
+                d - Rotate the world about the y-axis in negative direction
+                a - Rotate the world about the y-axis in positive direction
+                z - Rotate the world about the z-axis in negative direction                x - Rotate the world about the z-axis in positive direction
 
 
+            Movement:
+                e - Move forward
+                q - Move backward
+        """
+        self.keycodes: Dict[int, Dict[str, Any]] = dict([
+                (pg.K_w, {"func": lambda : self.rotate(-np.pi/180 * ROTATION_ANGLE, 0, 0)}),
+                (pg.K_s, {"func": lambda : self.rotate(np.pi/180 * ROTATION_ANGLE,0, 0)}),
+                (pg.K_d, {"func": lambda : self.rotate(0, -np.pi/180 * ROTATION_ANGLE, 0)}),
+                (pg.K_a, {"func": lambda : self.rotate(0, np.pi/180 * ROTATION_ANGLE, 0)}),
+                (pg.K_q, {"func": lambda : self.move(0, 0, MOVEMENT_DISTANCE)}),
+                (pg.K_e, {"func": lambda : self.move(0, 0, -MOVEMENT_DISTANCE)}), 
+                (pg.K_z, {"func": lambda : self.rotate(0, 0, -np.pi/180 * ROTATION_ANGLE)}),
+                (pg.K_x, {"func": lambda : self.rotate(0, 0, np.pi/180 * ROTATION_ANGLE)}), 
                 ])
+
         for key in self.keycodes:
-            self.keycodes[key].append(False)
+            self.keycodes[key]["flag"] = False
 
         pg.init()
         self.clock: pg.time.Clock = pg.time.Clock()
         self.display: pg.surface.Surface = pg.display.set_mode(SCREEN_SIZE)
 
-        self.entity = EntityRectangle((0,0,100), (100, 100, 100), (0,0,0))
 
-    def _rotate(self, x_rot: float, y_rot: float, z_rot: float) -> None:
-        self.entity.rotate(x_rot, y_rot, z_rot)
+        self.entities = [
+                Rectangle((50,0,0), (100, 100, 100), (0,0,0)),
+                Rectangle((-50,0,0), (100, 100, 100), (0,0,0)),
+                Rectangle((0,50,0), (100, 100, 100), (0,0,0)),
+                Rectangle((0,-50,0), (100, 100, 100), (0,0,0)),
+                ]
 
-    def _move(self, x_mov: float, y_mov: float, z_mov: float) -> None:
-        self.entity.move(x_mov, y_mov, z_mov)
+
+    def rotate(self, x_rot: float, y_rot: float, z_rot: float) -> None:
+        for entity in self.entities:
+            entity.rotate(x_rot, y_rot, z_rot)
+    
+    def move(self, x_mov: float, y_mov: float, z_mov: float) -> None:
+        for entity in self.entities:
+            entity.move(x_mov, y_mov, z_mov)
 
     def _processKeys(self) -> None:
-       
         for event in pg.event.get():
             if (event.type == pg.KEYDOWN or event.type == pg.KEYUP):
                 if (event.type == pg.KEYDOWN):
                     if (event.key in self.keycodes):
-                        self.keycodes[event.key][1] = True
+                        self.keycodes[event.key]["flag"] = True
                 elif (event.type == pg.KEYUP):
                     if (event.key in self.keycodes):
-                        self.keycodes[event.key][1] = False
+                        self.keycodes[event.key]["flag"] = False
 
         for key in self.keycodes:
             if (key in self.keycodes):
-                if (self.keycodes[key][1]):
-                    self.keycodes[key][0]()
+                if (self.keycodes[key]["flag"]):
+                    self.keycodes[key]["func"]()
 
     def run(self) -> None:
         while True:
             self._processKeys()
              
             self.display.fill((255,255,255))
-            self.entity.blit(self.display)
+            for entity in self.entities:
+                entity.blit(self.display)
             pg.display.update()
 
             self.clock.tick(60)
